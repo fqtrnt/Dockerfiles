c552f7569f7f524fe8c6f1e866d101970e8b83c9
fqtrnt <fqtrnt@gmail.com>
2017-02-13 10:03:37 +0800

    add jenkins witdh docker

---
+++ jenkins/with-docker/linux_start.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#docker run -it -p 8090:8080 -p 50000:50000 -v /media/e/dev/jdk/linux/8:/usr/local/jdk -v /media/e/docker/jenkins/workspace:/var/jenkins_home -v /media/e/dev/gradle:/var/gradle_home -v /media/e/dev/mvn:/var/maven_home -v /media/e/.gradle/:/var/.gradle -v /media/e/.m2/:/var/jenkins_home/.m2 jenkins:1.0 bash
+docker run -d -p 8090:8080 -p 50000:50000 \
+  -v /media/e/dev/jdk/linux/8:/usr/local/jdk \
+  -v /media/e/docker/jenkins/workspace:/var/jenkins_home \
+  -v /media/e/dev/gradle:/var/gradle_home \
+  -v /media/e/dev/mvn:/var/maven_home \
+  -v /media/e/.gradle/:/var/.gradle \
+  -v /media/e/.m2/:/var/jenkins_home/.m2 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(which docker):/usr/bin/docker \
+  jenkins:1.1
